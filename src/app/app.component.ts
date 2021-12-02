f1bad17d9c21b9dbed93728746eb3edba48f3a38
neryad <dayern.gomez@hotmail>
2021-12-01 21:45:22 -0400

    Co-authored-by: Ruslán González <ruslanguns@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { NewsService } from './shared/Services/news.service';
 import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { Articles } from './shared/interface/news.interface';
 
+import { ElementRef } from '@angular/core';
 type AvailableDiaries =
   | 'listin'
   | 'diarioLibre'
@@ -15,10 +17,13 @@ type AvailableDiaries =
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'local-rd-news';
   selectedDiary = 'listin';
 
+  articles!: { [key: string]: Articles[] };
+
+  // Creamos un View Model con el observable
   articlesVm$ = combineLatest([
     this.newsService.getNews('listin'),
     this.newsService.getNews('diarioLibre'),
@@ -28,20 +33,37 @@ export class AppComponent {
   ]).pipe(
     map(([listin, diarioLibre, nacional, nuevoDiario, remolacha]) => ({
       listin,
-      diarioLibre,
+      diarioLibre, // LO ESTABAS HACIENDO SUPER!
       nacional,
       nuevoDiario,
       remolacha,
-    }))
+    })), // insertamos los articulos...
+    tap((data) => (this.articles = data))
   );
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService, private el: ElementRef) {
+    this.refreshData(); // Cargamos los articulos al construirse
+  }
 
   setSelectedDiary(mediaName: AvailableDiaries) {
     this.selectedDiary = mediaName;
   }
 
   refreshData() {
-    this.articlesVm$.subscribe();
+    this.articlesVm$.subscribe(); // Refrescamos cuando sea necesario.
+  }
+
+  ngAfterViewInit(): void {
+    let hamburger = document.querySelector(
+      '.header .nav-bar .nav-list .hamburger'
+    );
+    let mobile_menu = this.el.nativeElement.querySelector(
+      '.header .nav-bar .nav-list ul'
+    );
+
+    hamburger!.addEventListener('click', () => {
+      hamburger!.classList.toggle('active');
+      mobile_menu.classList.toggle('active');
+    });
   }
 }
